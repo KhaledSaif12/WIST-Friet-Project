e9870c35c66f35fea728ec26b873c42fd36efa4e
Ahmed Alafoori <ahmedalafoori23@yahoo.com>
2024-08-07 00:40:01 +0300

    part-2

--- app/Models/Addresses.php
+++ app/Models/Addresses.php
@@ -7,11 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Addresses extends Model
 {
-    
 
-    
+    // تحديد العلاقة مع نموذج User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
-    
 
-    
+
+
+
 }
--- app/Models/Farms.php
+++ app/Models/Farms.php
@@ -4,14 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-
 class Farms extends Model
 {
-    
+    // تحديد العلاقة مع نموذج User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
-    
-
-    
-
-    
+    // تحديد العلاقة مع نموذج Addresses
+    public function address()
+    {
+        return $this->belongsTo(Addresses::class);
+    }
 }
