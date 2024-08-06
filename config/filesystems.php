555c5c59c46cc403b4e047095f7cd5c55ee939a3
Ahmed Alafoori <ahmedalafoori23@yahoo.com>
2024-08-06 07:50:56 +0300

    part-2

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,12 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+            // إضافة التكوين لجهاز التخزين admin
+    'admin' => [
+        'driver' => 'local',
+        'root' => storage_path('app/admin'),
+        'throw' => false,
+    ],
 
     ],
 
